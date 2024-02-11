877de31906bf0ccb97d07545a34af875be65a123
chinmayjainnnn <149592825+chinmayjainnnn@users.noreply.github.com>
2024-02-11 12:03:34 +0530

    Time: 82 ms (72.01%) | Memory: 61.9 MB (33.04%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        int s=0,e,mid,ans;
-        e=arr.size()-1;
-        return maxarr(arr,s,e);        
+        int n=arr.size();
+        int start=0,end=n-1;
+        int mid=-1;
+        while(start<=end){
+            mid=start+(end-start)/2;
+            if( (mid>0) && (arr[mid]>arr[mid-1] ) && (mid<n-1) && (arr[mid]>arr[mid+1] )){
+                return mid;
+            }
+            else if(  (mid>0) && (arr[mid]>arr[mid-1] )   ){
+                start=mid+1;
+            }
+            else if( (mid<n-1) && (arr[mid]>arr[mid+1] ) ){
+                end=end-1;
+            }
+            else cout<<"wtf"<<endl;
+        }
+
+    return mid;    
     }
-    int maxarr(vector<int>& arr,int s,int e)
-    {
-        int mid=(s+(e-s)/2);
-            if(arr[mid]>arr[mid-1])
-            {
-                if(arr[mid]>arr[mid+1]){return mid;}
-                return maxarr(arr,mid,e);
-            }
-            else 
-            {
-                return maxarr(arr,s,mid);
-            }
-     }
 };
