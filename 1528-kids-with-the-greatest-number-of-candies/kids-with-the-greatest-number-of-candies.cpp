2c82db7df13249a4f029f81d9547f03a38f315b3
chinmayjainnnn <149592825+chinmayjainnnn@users.noreply.github.com>
2024-02-15 23:12:10 +0530

    Time: 6 ms (19.28%) | Memory: 11 MB (11.55%) - LeetSync

---
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+       vector<bool> arr;
+       int max=0;
+       for(auto i:candies){
+           if(max<i)max=i;
+       } 
+       cout<< max<<endl;
+       int n=candies.size();
+       for(int i=0;i<n;i++){
+           if(max <= candies[i] + extraCandies){
+               arr.push_back(true);
+           }
+           else arr.push_back(false);
+
+       }
+       return arr;
+    }
+};
