ec2f0a66cc5565d92e6d52db066600c67164b774
chinmayjainnnn <149592825+chinmayjainnnn@users.noreply.github.com>
2024-04-24 14:49:53 +0530

    Time: 7 ms (67.59%) | Memory: 10.4 MB (29.15%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -3,17 +3,10 @@ public:
     bool checkInclusion(string s1, string s2) {
         int n=s1.size();
         int n2=s2.size();
-        map <char,int> h1;
-        map <char,int> h2;
+        unordered_map <char,int> h1;
+        unordered_map <char,int> h2;
 
-        // map <char,int> h3;
-        // map <char,int> h4;
         
-        // h3['a']=1;
-        // h4['a']=1;
-        // if(h3==h4){
-        //     cout<<"gaj";
-        // }
         if(n>n2)return 0;
         for(int i=0;i<n;i++){
             h1[s1[i]]+=1;
