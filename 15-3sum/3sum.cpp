45b78c42658b2fa15627ed6eeb4dc5587122eeb3
chinmayjainnnn <149592825+chinmayjainnnn@users.noreply.github.com>
2024-05-14 18:02:44 +0530

    Time: 71 ms (50.74%) | Memory: 27.1 MB (80.38%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -2,82 +2,33 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         int n=nums.size();
-        
-        unordered_map<int,int> ump;
+        sort(nums.begin(),nums.end());
         vector<vector<int>> ans;
-        map<vector<int>,bool> has;
-        int temp=1;
-        for(int i=0;i<n;i++){
-            int tar=-nums[i];
-            // temp=1;
-            for(int j=i-1;j>=0;j--){
-                int target=tar-nums[j];
-                if(   !temp &&  ( i>0 && nums[i]==nums[i-1] )  &&   ( j>0 &&  nums[j]==nums[j-1])   ){break;}
-                vector<int> v1;
-                if(ump.find(target) != ump.end() &&  (i !=j)  && (j != ump[target]) && (i!=ump[target])){
-                    v1.push_back(nums[i]);
-                    v1.push_back(target);
-                    v1.push_back(nums[j]);
-                    sort(v1.begin(),v1.end());
-                    if(!has[v1]){
-                        ans.push_back(v1);
-                        has[v1]=true;
-                        temp=1;
+        // unordered_map
 
+        for(int i=0;i<n-2;i++){
+            if((i==0)||(i<n && nums[i]!=nums[i-1] ) ){
+                int k=n-1,sum,j=i+1;
+
+                while(j<k){
+                    sum=nums[i]+nums[j]+nums[k];
+                    if(sum>0){
+                        while(k>j && nums[k-1]==nums[k]){k--;}
+                        k--;
                     }
-                    else{temp=0;}
-                }   
+                    else if(sum<0){
+                        while(j<k && nums[j]==nums[j+1]){j++;}
+                        j++;
+                    }
+                    else{
+                        ans.push_back({nums[i],nums[j],nums[k]});
+                        while(k>j &&nums[k-1]==nums[k]){k--;}
+                        k--;
+                        while(j<k && nums[j]==nums[j+1]){j++;}
+                        j++;
+                    }
+                }
             }
-            ump[nums[i]]=i;
         }
-    return ans;
-    }
+    return ans;}
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// int n=nums.size();
-//         unordered_map<int,int> arr;
-//         vector<vector<int>> ans;
-//         // unordered_map<vector<int>,int> t;
-        
-//         for(int i=0;i<n;i++){
-            
-//             int target=-nums[i];
-//             arr[-target]++;
-//             for(int j=0;j<i;j++){
-//                 int temp=target-nums[j];
-//                 vector<int> v1;
-//                 if(arr.find(temp)!=arr.end()){
-//                     v1.push_back(-target);
-//                     v1.push_back(temp);
-//                     v1.push_back(nums[j]);
-//                     sort(v1.begin(),v1.end());
-                    
-//                     ans.push_back(v1);
-
-//                 }
-
-//             }
-//         }
-//     return ans;
