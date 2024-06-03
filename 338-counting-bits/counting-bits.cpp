eb59c1a0bbe6b0dc2979a2c6e9fdabcde3fe8fd9
chinmayjainnnn <149592825+chinmayjainnnn@users.noreply.github.com>
2024-06-03 15:00:00 +0530

    Time: 3 ms (72.05%) | Memory: 9 MB (80.26%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,24 +1,9 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-    if(n==0)return {0};
-    vector<int> v;
-    v.push_back(0);
-    
-    v.push_back(1);
-
-    int temp=2;
-    
-
-    for(int i=2;i<=n;i++){
-        if(i==temp*2){
-            temp=temp*2;
-        }
-        
-        v.push_back(v[i-temp]+1);
-
-    }
-    
-    return v;
+        vector<int>dp(n+1,0);
+        for(int i=0;i<=n;i++)
+            dp[i] = (i%2) + dp[i/2];
+        return dp;
     }
 };
