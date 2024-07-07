e714e845be784df6b36d8c1cd3d52933b596a8b2
chinmayjainnnn <149592825+chinmayjainnnn@users.noreply.github.com>
2024-07-07 14:35:19 +0530

    Time: 4 ms (51.58%) | Memory: 13.4 MB (41.95%) - LeetSync

--- 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.cpp
+++ 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.cpp
@@ -3,11 +3,9 @@ public:
     int minimumCost(vector<int>& cost) {
         sort(cost.begin(),cost.end());
         int n=cost.size(),t=0,sum=0;
-        cout<<n<<endl;
         for(int i=n-1;i>=0;i--,t++){
             if((t+1)%3!=0){
                 sum=sum+cost[i];
-                cout<<sum<<" ";
             }
             
         }
