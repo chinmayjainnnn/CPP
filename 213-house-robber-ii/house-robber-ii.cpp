3881e491e5b075bb54b811e919a4e6031325c1b2
chinmayjainnnn <149592825+chinmayjainnnn@users.noreply.github.com>
2024-08-21 16:16:11 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (25.89%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,42 +1,28 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
+    int robb(vector<int>& nums,int start,int end) {
+        vector<int> dp(nums.size(),0);
         
-        int n = nums.size();
-        if(n==1)return nums[0];
-        if(n==2)return max(nums[0],nums[1]);
-        if(n<=3){
-            return max(nums[0],max(nums[1],nums[2]));
-        }
-        
-        vector<int> dp(n);
-        
-        for(int i=0;i<n-1;i++){
-            if(i<2){
+        for(int i=start;i<end;i++){
+            if(i<2+start){
                 dp[i]=nums[i];
                 continue;
             }
-            if(i==2){dp[2]=dp[0]+nums[2];continue;}
-            // temp=max()
-            dp[i]=nums[i]+ max(dp[i-2],dp[i-3]) ;
+            if(i==2+start){dp[start+2]=dp[start]+nums[i];continue;}
             
+            dp[i]=nums[i]+ max(dp[i-2],dp[i-3]) ;
         }
         
-        int maxi=max(dp[n-3],dp[n-2]);
-        dp[0]=0;
-        for(int i=1;i<n;i++){
-            if(i<3){
-                dp[i]=nums[i];
-                continue;
-            }
-            if(i==3){dp[3]=dp[1]+nums[3];continue;}
-            dp[i]=nums[i]+ max(dp[i-2],dp[i-3]) ;
-            
-        }
-
-        maxi=max(maxi,max(dp[n-1],dp[n-2]));
-
-        return maxi;
+        return max(dp[end-1],dp[end-2]);
     }
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1)return nums[0];
+        if(n==2)return max(nums[1],nums[0]);
+        if(n==3)return max(nums[0],max(nums[1],nums[2]));
+        
+        return max(robb(nums,0,n-1),robb(nums,1,n));
+    }
+
     
 };
