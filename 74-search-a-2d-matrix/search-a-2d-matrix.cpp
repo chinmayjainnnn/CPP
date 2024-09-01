7353bc89f787b07220fc4f85e444f72917ede9bd
chinmayjainnnn <149592825+chinmayjainnnn@users.noreply.github.com>
2024-09-01 17:08:37 +0530

    Time: 7 ms (22.15%) | Memory: 12.3 MB (37.09%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,38 +1,35 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-    int n=matrix.size();
-    int m=matrix[0].size();
+        int m=matrix.size(),n=matrix[0].size();
+        int start=0,end=m-1,mid,row,temp=0;
 
-    int start=0,end=n-1,mid;
-    mid=start+(end-start)/2;
-    while(mid>-1 && mid<n && start<=end){
-        mid=start+(end-start)/2;
+        while(start<=end){
+            mid=start+(end-start)/2;
+            if(matrix[mid][0]==target)return true;
+            if(matrix[mid][0] <=target && matrix[mid][n-1]>=target){
+                temp=1;
+                row =mid;
+                break;
+            }    
+            else if(matrix[mid][0] <=target)start=mid+1;
+            else end=mid-1;
+            
+        } 
+        if(!temp)return 0;
+        
+        start=0;
+        end=n-1;
+        // cout<<row<<endl;
+        while(start<=end ){
+            // cout<<start<<" "<<mid<<" "<<end<<endl;
+            mid=start+(end-start)/2;
+            if(matrix[row][mid]==target)return true;
+            else if(matrix[row][mid]<target)start=mid+1;
+            else end=mid-1;
+        }
 
-        if(mid>-1 && mid<n && matrix[mid][0] == target)return true;
-        else if(mid>-1 && mid<n && matrix[mid][0] <target  ){
-            start=mid+1;
+
+        return 0;
         }
-        else{
-            end=mid-1;
-        }
-    }
-    int temp=end;
-    start=0;
-    end=m-1;
-    mid=start+(end-start)/2;
-    if(temp<0 || temp >n-1)return false;
-    while(mid>-1 && mid<m && start<=end){
-        mid=start+(end-start)/2;
-        if(mid>-1 && mid<m && matrix[temp][mid] == target)return true;
-        else if(mid>-1 && mid<m && matrix[temp][mid]<target){
-            start=mid+1;
-        }
-        else{
-            end=mid-1;
-        }
-    }
-    
-    return 0;
-    }
 };
