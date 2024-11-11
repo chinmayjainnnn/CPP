16ca7f48761ba80454f13c94a8c5e9e30c3a983c
chinmayjainnnn <149592825+chinmayjainnnn@users.noreply.github.com>
2024-11-11 15:06:04 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (48.96%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -2,32 +2,19 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int n=nums.size();
-        int peak=-1;
-        int temp=0;
-        for(int i=n-1;i>0;i--){
-            if(nums[i] > nums[i-1]  ){
-                peak=i;
-                break;
-            }
-        }
-        
-        if(peak==-1){
+        int peak=n-1;
+
+        while(peak>0 && nums[peak]<=nums[peak-1])peak--;
+        if(peak==0){
             sort(nums.begin(),nums.end());
             return;
         }
-        int x;
-
-        for(int i=n-1;i>=peak;i--){
-            if(nums[i] >nums[peak-1]   ){
-                x=i;
-                break;
-            }
-        }
-        temp=nums[x];
-        nums[x]=nums[peak-1];
-        nums[peak-1]=temp;
-        sort(nums.begin()+peak,nums.end() );
-        return;
-
+        int i=n-1;
+        while(i>peak && nums[peak-1]>=nums[i])i--;
+        
+        // cout<<peak<<" " <<i<<endl;
+        swap(nums[peak-1],nums[i]);
+        sort((nums.begin()+peak),nums.end());
+        
     }
 };
