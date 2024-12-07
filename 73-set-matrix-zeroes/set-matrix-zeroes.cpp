2186df6f71e6eb0e05859574bfbb12d94421fe3a
Chinmay Jain <149592825+chinmayjainnnn@users.noreply.github.com>
2024-12-07 07:45:47 +0530

    Time: 0 ms (100.00%) | Memory: 18.8 MB (7.41%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,36 +1,26 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int m=matrix.size();
-        int n=matrix[0].size();
-        
+        int m=matrix.size(),n=matrix[0].size();
         vector<int> row(m,1);
-        vector<int> column(n,1);
-        
+        vector<int> col(n,1);
+
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(matrix[i][j]==0){
                     row[i]=0;
-                    column[j]=0;
+                    col[j]=0;
                 }
             }
         }
-        int temp;
         for(int i=0;i<m;i++){
-            temp=row[i];
             for(int j=0;j<n;j++){
-                matrix[i][j]=matrix[i][j]*temp*column[j];
-        
+                if(!row[i] || !col[j]){
+                    matrix[i][j]=0;
+                }
             }
         }
 
-
-        // for(int i=0;i<m;i++){
-        //     cout<<row[i]<< " ";
-        // }
-        // cout<<endl;
-        // for(int i=0;i<n;i++){
-        //     cout<<column[i]<< " ";
-        // }
+    // return 
     }
 };
