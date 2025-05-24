9e1f0d856d4b8e06788f7a31b40a4b4288ca41ee
Chinmay Jain <149592825+chinmayjainnnn@users.noreply.github.com>
2025-05-25 01:17:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (10.74%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -3,7 +3,7 @@ public:
     int hammingWeight(int n) {
         int ans=0;
         while(n>0){
-            if(n%2==1)ans+=1;
+            ans+=n%2;
             n=n/2;
         }
         return ans;
