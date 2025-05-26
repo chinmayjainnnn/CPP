0a96d54c348a3bcd04b18fdcb579e4a0d63a3ecd
Chinmay Jain <149592825+chinmayjainnnn@users.noreply.github.com>
2025-05-26 11:51:44 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (23.48%) - LeetSync

--- 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int bitwiseComplement(int n) {
-        if(n==0){return 1;}
-        int rem,comp=0,i=0,dec=0;
-        while(n!=0)
-        {
-            rem=1-(n&1);
-            dec=dec+(rem)*(pow(2,i));
-            i++;
-            n=n>>1;
+        if(n==0)return 1;
+        int temp=n,digit=0;
+        while(n!=0){
+            digit+=1;
+            n=n/2;
         }
-        return dec;
+
+        return pow(2,digit)-1-temp;
     }
 };
