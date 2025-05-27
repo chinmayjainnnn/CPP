4ee0e506919374b2f87820eb08303dc8f2797985
Chinmay Jain <149592825+chinmayjainnnn@users.noreply.github.com>
2025-05-27 15:00:12 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (70.35%) - LeetSync

--- 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.cpp
+++ 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int differenceOfSums(int n, int m) {
-        int temp=(n*(n+1))/2;
-        int pro=m*(n/m)*(n/m +1);
-        return temp-pro;
+        int temp=n/m;
+        return (n*(n+1))/2-m*(temp*(temp +1));
     }
 };
