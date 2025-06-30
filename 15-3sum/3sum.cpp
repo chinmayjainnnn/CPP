9d8361a07abc2dce86c19a8e9dec579e5542e168
Chinmay Jain <149592825+chinmayjainnnn@users.noreply.github.com>
2025-06-30 16:00:16 +0530

    Time: 47 ms (71.45%) | Memory: 29.2 MB (42.02%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -7,14 +7,14 @@ public:
         int temp=0;
         for(int i=0;i<n-2;i++){
             if(i>0 && nums[i]==nums[i-1])continue;
-            int target= -nums[i];
+            
 
             int l=i+1,r=n-1;
             
             while(l<r){
-                // cout<<i<<" "<<l<<" "<<r<<endl;
-                if(nums[l]+nums[r]==target){
-                    ans.push_back({-target,nums[l],nums[r]});
+                int sum=nums[i]+nums[l]+nums[r];
+                if(sum==0){
+                    ans.push_back({nums[i],nums[l],nums[r]});
                     
                     l++;
                     r--;
@@ -22,7 +22,7 @@ public:
                     while(r>l && nums[r]==nums[r+1])r--;
                     
                 }
-                else if(nums[l]+nums[r]<target)l++;
+                else if(sum<0)l++;
                 else r--;
             }
             
