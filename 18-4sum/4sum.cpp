ce70c145fb531384674f625ed79dfbb6a71b9410
Chinmay Jain <149592825+chinmayjainnnn@users.noreply.github.com>
2025-06-30 16:29:47 +0530

    Time: 23 ms (57.58%) | Memory: 17.3 MB (84.89%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,38 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        int n=nums.size();
-
         sort(nums.begin(),nums.end());
+        int n=nums.size();
         vector<vector<int>> ans;
-        
+
         for(int i=0;i<n-3;i++){
-            if(i==0|| (i>0 && nums[i]!=nums[i-1] )){
-                long long three=target-nums[i];
-                for(int j=i+1;j<n-2;j++){
-                    if(j==i+1|| nums[j]!=nums[j-1]){
-                        int k=j+1,l=n-1;
-                        long long two=three-nums[j];
+            if(i>0 && nums[i]==nums[i-1])continue;
+            for(int j=i+1;j<n-2;j++){
 
-                        while(k<l){
-                            if((long long)(nums[k]+nums[l])==two){
-                                ans.push_back({nums[i],nums[j],nums[k],nums[l]});
-                                k++;
-                                l--;
-                                while((k<l) && nums[k]==nums[k-1]) k++;
-                                while((l>k) && nums[l]==nums[l+1]) l--;
-                            }
-                            else if(nums[k]+nums[l]>two){
-                                l--;
-                            }
-                            else{
-                                k++;
-                            }
-
-                        }
-                    }   
+                if(j>i+1 && nums[j]==nums[j-1])continue;
+                int left=j+1,right=n-1;
+                while(left<right){
+                    long long sum=(long long)nums[i]+nums[j]+nums[left]+nums[right];
+                    if(sum>target)right--;
+                    else if(sum<target)left++;
+                    else{
+                        ans.push_back({nums[i],nums[j],nums[left],nums[right]});
+                        left++;
+                        right--;
+                        while(left<right && nums[left]==nums[left-1])left++;
+                        while(right>left && nums[right]==nums[right+1])right--;
+                    }
+                    // cout<<i<<" "<<j<<" "<<left<<" "<<right<<endl;
                 }
+
             }
         }
-    return ans;}
+        return ans;
+    }
 };
