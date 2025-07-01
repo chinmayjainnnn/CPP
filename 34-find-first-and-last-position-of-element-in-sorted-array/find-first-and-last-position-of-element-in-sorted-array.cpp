cf5cb6df830365d55038f282abae2172cab008eb
Chinmay Jain <149592825+chinmayjainnnn@users.noreply.github.com>
2025-07-01 18:21:44 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (17.45%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,41 +1,56 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        int n =nums.size();
+    vector<int> searchRange(vector<int>& arr, int k) {
+        
+        int n=arr.size();
+        if(n==0)return {-1,-1};
+        vector<int> ans;
+        int f=-1,b=-1;
         int start=0,end=n-1,mid;
-        int temp=-1;
-        vector <int> ans;
+        if(arr[0]==k){
+            start=n;
+            f=0;
+        }
         
         while(start<=end){
             mid=start+(end-start)/2;
-            if(target>nums[mid]){
+            if(mid>0 && arr[mid]==k && arr[mid-1]!=k ){
+                f=mid;
+                break;
+            }
+            else if(arr[mid]<k ){
                 start=mid+1;
             }
-            else{
+            else if(arr[mid]>k || (mid>0 && arr[mid]==k && arr[mid-1]==k)){
                 end=mid-1;
-                if(target==nums[mid]){
-                    temp=mid;
-                }
             }
         }
-        ans.push_back(temp);
-        
         start=0;
         end=n-1;
-        temp=-1;
+        if(arr[n-1]==k){
+            start=n;
+            b=n-1;
+        }
         while(start<=end){
             mid=start+(end-start)/2;
-            if(target<nums[mid]){
+            if(mid<n-1 && arr[mid]==k && arr[mid+1]!=k){
+                b=mid;
+                break;
+            }
+            else if(arr[mid]<k || (mid<n-1 && arr[mid+1]==k && arr[mid]==k)){
+                start=mid+1;
+            }
+            else if(arr[mid]>k){
                 end=mid-1;
             }
-            else{
-                start=mid+1;
-                if(target==nums[mid]){
-                    temp=mid;
-                }
-            }
         }
-        ans.push_back(temp);
+
+
+
+
+    ans.push_back(f);
+    ans.push_back(b);
     return ans;
     }
+
 };
