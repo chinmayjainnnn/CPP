29114afebeda3aaaa30ca0d754d522a1cb453966
Chinmay Jain <149592825+chinmayjainnnn@users.noreply.github.com>
2025-07-05 15:35:49 +0530

    Time: 35 ms (25.85%) | Memory: 88.3 MB (65.87%) - LeetSync

---
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool check(vector<int>& candies, long long k,int max){
+        long long temp=0;
+
+        for(int i=0;i<candies.size();i++){
+            int num=candies[i]/max;
+            temp+=num;
+            if(temp>=k)return true;    
+        }
+        
+        return false;
+    }
+
+
+    int maximumCandies(vector<int>& candies, long long k) {
+        long long total=0;
+        for(int i=0;i<candies.size();i++){
+            total+=candies[i];
+        }
+        if(k>=total){
+            return total/k;
+        }
+        long long s=0,e=total,mid;
+        int ans=0;
+        while(s<=e){
+            mid=s+(e-s)/2;
+            if(mid==0)return 1;
+            // cout<<s<<" "<<mid<<" "<<e;
+            if(check(candies,k,mid) ){
+                ans=mid;
+                s=mid+1;
+                // cout<<"fit"<<endl;   
+            }
+            else{
+                e=mid-1;
+                // cout<<"unfit"<<endl;
+            }
+
+        }
+        
+        return ans;
+    }
+};
