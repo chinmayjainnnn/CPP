274e455d7e40226679ae71cb9fbd7cc9b22ef426
Chinmay Jain <149592825+chinmayjainnnn@users.noreply.github.com>
2025-10-10 11:26:31 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (35.42%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -3,15 +3,27 @@ public:
     void rotate(vector<vector<int>>& arr) {
         int n=arr.size();
         
-        for(int d=0;d<n/2;d++){
-            for(int e=d;e<n-d-1;e++){
-                int temp=arr[d][e];
-                arr[d][e]=arr[n-1-e][d];
-                arr[n-1-e][d]=arr[n-d-1][n-e-1];
-                arr[n-d-1][n-e-1]=arr[e][n-d-1];
-                arr[e][n-d-1]=temp;
-                
+        for(int k=0;k<n/2;k++){
+            for(int i=k;i<n-k-1;i++){
+                int temp=arr[k][i];
+                arr[k][i]=arr[n-i-1][k];
+                arr[n-i-1][k]=arr[n-k-1][n-1-i];
+                arr[n-k-1][n-1-i]=arr[i][n-k-1];
+                arr[i][n-k-1]=temp;                
             }
         }
+       
     }
 };
+
+
+// for(int k=0;k<n/2;k++){
+//             for(int i=k;i<n-k-1;i++){
+//                 int temp=arr[k][i];
+//                 arr[k][i]=arr[n-1-i][k];
+//                 arr[n-1-i][k]=arr[n-k-1][n-i-1];
+//                 arr[n-k-1][n-i-1]=arr[i][n-k-1];
+//                 arr[i][n-k-1]=temp;
+                
+        //     }
+        // }
