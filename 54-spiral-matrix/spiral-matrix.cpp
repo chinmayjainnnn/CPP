5befe8185d3ef5554fb76e364fb054be4936e7e3
Chinmay Jain <149592825+chinmayjainnnn@users.noreply.github.com>
2025-10-18 10:40:56 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (78.85%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -8,36 +8,36 @@ public:
             int horizontal=k;
 
             for(int i=k;i<m-k;i++,count++){
-                cout<<horizontal<<" "<<i<<" ";
+                // cout<<horizontal<<" "<<i<<" ";
                 ans.push_back(matrix[horizontal][i]);
             }
-            cout<<endl;
-            cout<<count<<endl;
+            // cout<<endl;
+            // cout<<count<<endl;
             int vertical=m-k-1;
             if(count>=max)break;
             for(int j=k+1;j<n-k;j++,count++){
-                cout<<j<<" "<<vertical<<" ";
+                // cout<<j<<" "<<vertical<<" ";
                 ans.push_back(matrix[j][vertical]);
             }
             horizontal=n-k-1;
-            cout<<endl;
-            cout<<count<<endl;
+            // cout<<endl;
+            // cout<<count<<endl;
             if(count>=max)break;
             for(int i=m-k-1-1;i>k;i--,count++){
-                cout<<horizontal<<" "<<i<<" ";
+                // cout<<horizontal<<" "<<i<<" ";
                 ans.push_back(matrix[horizontal][i]);
             }
             vertical=k;
-            cout<<endl;
-            cout<<count<<endl;
+            // cout<<endl;
+            // cout<<count<<endl;
             if(count>=max)break;
             for(int j=n-k-1;j>k;j--,count++){
-                cout<<j<<" "<<vertical<<" ";
+                // cout<<j<<" "<<vertical<<" ";
                 ans.push_back(matrix[j][vertical]);
             }
             if(count>=max)break;
-            cout<<endl;
-            cout<<count<<endl;
+            // cout<<endl;
+            // cout<<count<<endl;
         }
         return ans;
     }
