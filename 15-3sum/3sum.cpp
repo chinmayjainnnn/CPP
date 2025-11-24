4bc7fd784d4a3854bad8f3e2d2d65cba1db64a1f
Chinmay Jain <149592825+chinmayjainnnn@users.noreply.github.com>
2025-11-24 13:20:16 +0530

    Time: 39 ms (94.05%) | Memory: 29.1 MB (71.88%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,36 +1,40 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        vector<vector<int>> ans;
         int n=nums.size();
-        int temp=0;
-        for(int i=0;i<n-2;i++){
-            if(i>0 && nums[i]==nums[i-1])continue;
+        unordered_map<int,int> mp;
+        vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
+        if( n<3 | nums[0]>0)return {};
+        for(int i=0;i<nums.size()-2;i++){
+            if(i>0 && nums[i-1]==nums[i])continue;
+            int a=nums[i];
             
-
-            int l=i+1,r=n-1;
-            
-            while(l<r){
-                int sum=nums[i]+nums[l]+nums[r];
+            int left=i+1,right=n-1;
+            while(left<right){
+                // cout<< a<<" "<<nums[left]<<" "<<nums[right]<<endl;
+                int sum=a+nums[left]+nums[right];
                 if(sum==0){
-                    ans.push_back({nums[i],nums[l],nums[r]});
-                    
-                    l++;
-                    r--;
-                    while(l<n && nums[l]==nums[l-1])l++;
-                    while(r>l && nums[r]==nums[r+1])r--;
+                    ans.push_back({nums[i],nums[left],nums[right]});
+                    while(left<right &&nums[left]==nums[left+1]){
+                        left++;
+                    }
+                    while(left<right &&nums[right]==nums[right-1]){
+                        right--;
+                    }
+                    left++;
+                    right--;
                     
                 }
-                else if(sum<0)l++;
-                else r--;
+                else if(sum<0){
+                    left++;
+                }
+                else{
+                    right--;
+                }
             }
-            
-
         }
-    return ans;
+
+        return ans;
     }
 };
-auto init = atexit([]() {
-    ofstream("display_runtime.txt") << "0";
-});
