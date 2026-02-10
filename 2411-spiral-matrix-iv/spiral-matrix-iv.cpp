d42589947782b88f3f4765886ef6e6afa314d1f3
Chinmay Jain <149592825+chinmayjainnnn@users.noreply.github.com>
2026-02-10 21:12:04 +0530

    Time: 18 ms (66.25%) | Memory: 138.1 MB (62.55%) - LeetSync

---
+++ 2411-spiral-matrix-iv/spiral-matrix-iv.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> spiralMatrix(int m, int n, ListNode* head) {
+        vector<vector<int>> ans(m,vector<int>(n,-1));
+        
+        int top=0,left=0,bottom=m-1,right=n-1;
+        while(head!=NULL){
+            
+                // cout<<"first"<<endl;
+                for(int j=left;j<=right && head!=NULL;j++){
+                    ans[top][j]=head->val;
+                    head=head->next;
+                    // cout<<top<<" "<<j<<endl;
+                }
+                
+                top++;
+                // cout<<"Second"<<endl;
+                for(int i=top;i<=bottom && head!=NULL;i++){
+                    ans[i][right]=head->val;
+                    head=head->next;
+                    // cout<<i<<" "<<right<<endl;
+                }
+                right--;
+                // cout<<"Third"<<endl;
+                for(int j=right;j>=left && head!=NULL;j--){
+                    ans[bottom][j]=head->val;
+                    head=head->next;
+                }
+                bottom--;
+                for(int i=bottom;i>=top  && head!=NULL;i--){
+                    ans[i][left]=head->val;
+                    head=head->next;
+                }
+                left++;
+            
+        }
+    return ans;}
+};
