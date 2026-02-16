fbf1ae27d4dccbc3d748bd07e55cac7a61157533
Chinmay Jain <149592825+chinmayjainnnn@users.noreply.github.com>
2026-02-16 11:24:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (15.72%) - LeetSync

--- 190-reverse-bits/reverse-bits.cpp
+++ 190-reverse-bits/reverse-bits.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
-    uint32_t reverseBits(uint32_t n) {
-        long int rev=0;
-        for(int i=0;i<32;i++)
-        {
-            rev= (2*rev) +(n&1);
-            n=n>>1;
+     unsigned int reverseBits(unsigned int n) {
+        n=n/2;
+        unsigned int ans=0;
+        for(int i=0;i<31 ;i++ ){
+            ans= ans*2 +n%2;
+            n=n/2;
         }
-        return rev;
+
+        return ans;
     }
 };
