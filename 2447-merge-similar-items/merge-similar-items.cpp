ea257417d15435103d08f8d2e68af749effac45e
Chinmay Jain <149592825+chinmayjainnnn@users.noreply.github.com>
2026-02-28 15:59:58 +0530

    Time: 11 ms (23.60%) | Memory: 22 MB (6.86%) - LeetSync

--- 2447-merge-similar-items/merge-similar-items.cpp
+++ 2447-merge-similar-items/merge-similar-items.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     vector<vector<int>> mergeSimilarItems(vector<vector<int>>& items1, vector<vector<int>>& items2) {
         map<int,int> mp;
-        for(auto v:items1){
+        for(vector<int> v:items1){
             mp[v[0]]+=v[1];
         }
-        for(auto v:items2){
+        for(vector<int> v:items2){
             mp[v[0]]+=v[1];
         }
         vector<vector<int>> ans;
