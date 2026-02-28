979f2a86abf66ba96f08e8d06c592ac6c55e9c0e
Chinmay Jain <149592825+chinmayjainnnn@users.noreply.github.com>
2026-02-28 11:50:33 +0530

    Time: 27 ms (75.17%) | Memory: 8.1 MB (41.61%) - LeetSync

--- 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
@@ -6,9 +6,7 @@ public:
         long long mod = 1e9 + 7;
         
         for(int i=2;i<n+1;i++){
-            if(!(i & i-1)){
-                bits+=1;
-            }
+            if(!(i & i-1))bits++;
             ans=((ans<<bits)|i)%mod;
         }
         return ans;
