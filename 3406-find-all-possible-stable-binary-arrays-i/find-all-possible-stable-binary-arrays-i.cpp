0f8e984838bda157fbd1d0cda2caab5dbe5c540f
Chinmay Jain <149592825+chinmayjainnnn@users.noreply.github.com>
2026-03-09 12:35:19 +0530

    Time: 472 ms (26.02%) | Memory: 51.2 MB (34.15%) - LeetSync

---
+++ 3406-find-all-possible-stable-binary-arrays-i/find-all-possible-stable-binary-arrays-i.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    long long rec(int r_zeros,int r_ones,int last,int limit,vector<vector<vector<long long>>> &mem){
+        if(mem[r_zeros][r_ones][last]!=-1)return mem[r_zeros][r_ones][last]; 
+        if(r_zeros==0 && r_ones==0){return 1;}
+        long long ans=0;
+        long long mod=1e9+7;
+        if(last==0){
+            for(int i=1;i<=min(r_ones,limit);i++){
+                ans+=(rec(r_zeros,r_ones-i,1,limit,mem))%mod;
+            }
+        }
+        else{
+            for(int i=1;i<=min(r_zeros,limit);i++){
+                ans+=(rec(r_zeros-i,r_ones,0,limit,mem))%mod;
+            }
+        }
+        mem[r_zeros][r_ones][last]=ans;
+        return ans%mod;
+        
+
+    }
+    int numberOfStableArrays(int zero, int one, int limit) {
+        long long ans=0;
+        long long mod=1e9+7;
+        vector<vector<vector<long long>>> mem(zero+1,vector<vector<long long>>(one+1,vector<long long>(2,-1)));
+        ans+=rec(zero,one,1,limit,mem);
+        ans+=rec(zero,one,0,limit,mem);
+        return ans%mod;
+    }
+};
