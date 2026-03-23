7d1ba27d032195eb0ab0d200f064c638405b8600
Chinmay Jain <149592825+chinmayjainnnn@users.noreply.github.com>
2026-03-23 22:25:18 +0530

    Time: 556 ms (90.08%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -1,5 +1,10 @@
 # Write your MySQL query statement below
-delete p1
-from Person as p1
-join person as p2 on p1.email =p2.email
-where p1.id>p2.id
+delete 
+from Person as p1 
+where id not in (
+    select id from(
+        select min(id) as id
+        from Person 
+        group by email
+    ) as p2
+)
