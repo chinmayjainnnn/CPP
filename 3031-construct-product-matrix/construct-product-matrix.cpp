6eb7a11f77e14431e6458aed1b594d80c81b562b
Chinmay Jain <149592825+chinmayjainnnn@users.noreply.github.com>
2026-03-24 15:29:26 +0530

    Time: 25 ms (89.67%) | Memory: 132.1 MB (71.96%) - LeetSync

--- 3031-construct-product-matrix/construct-product-matrix.cpp
+++ 3031-construct-product-matrix/construct-product-matrix.cpp
@@ -3,28 +3,22 @@ public:
     vector<vector<int>> constructProductMatrix(vector<vector<int>>& grid) {
         int n=grid.size(),m=grid[0].size();
         vector<vector<int>> ans(n,vector<int>(m));
-        vector<long > suf(n*m,1);
-        vector<long > pre(n*m,1);
-        
-        for(int i=1;i<n*m;i++){
-            
-            int x=(i-1)/m;
-            int y=(i-1)%m;
-            
-            pre[i]=(pre[i-1]*(grid[x][y]))%12345;
-            // cout<<i<<" "<<pre[i]<<endl;
-        }
-        for(int i=n*m-2;i>=0;i--){
-            int x=(i+1)/m;
-            int y=(i+1)%m;
-            suf[i]=(suf[i+1]*grid[x][y])%12345;
-        }
-
+        long pre=1;
+        long suf=1;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                ans[i][j]=(pre[i*m+j]*suf[i*m+j])%12345;
+                ans[i][j]=pre;
+                pre=(pre*grid[i][j])%12345;
             }
         }
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                ans[i][j]=(ans[i][j]*suf)%12345;
+                suf=(suf*grid[i][j])%12345;
+            }
+        }
+
+
         return ans;
 
     }
