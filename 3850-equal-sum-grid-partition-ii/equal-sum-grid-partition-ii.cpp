09ed6ddc529a181b978858086669f80b07fa7dac
Chinmay Jain <149592825+chinmayjainnnn@users.noreply.github.com>
2026-03-26 12:19:55 +0530

    Time: 1013 ms (49.23%) | Memory: 404.4 MB (47.69%) - LeetSync

---
+++ 3850-equal-sum-grid-partition-ii/equal-sum-grid-partition-ii.cpp
@@ -0,0 +1,86 @@
+class Solution {
+public:
+    bool canPartitionGrid(vector<vector<int>>& grid) {
+        int n=grid.size(),m=grid[0].size();
+        long total=0;
+        unordered_map<long,int> top,bottom;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                int val=grid[i][j];
+                total+=val;
+                bottom[val]+=1;
+            }
+        }
+        long sum1=0;
+        long sum2=total;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                int val=grid[i][j];
+                sum1+=val;
+                sum2-=val;
+                top[val]++;
+                bottom[val]--;
+                if(bottom[val]==0)bottom.erase(val);
+            }
+            if(sum1==sum2)return true;
+            long dif=abs(sum1-sum2);
+            if(sum1>sum2){
+                if(top.find(dif)!=top.end()){
+                    if(m!=1){
+                        if(i!=0)return true;
+                        if(grid[0][0]==dif || grid[0][m-1]==dif)return true;
+                    }   
+                    else if(grid[0][0]==dif || grid[i][0]==dif)return true;             
+                }
+            }
+            else if( bottom.find(dif)!=bottom.end()){
+                if(m!=1 ){
+                   if(i!=n-2)return true;
+                   cout<<"here"<<endl;
+                   if(grid[n-1][0]==dif||grid[n-1][m-1]==dif)return true;                    
+                }
+                else if(grid[i+1][0]==dif || grid[n-1][0]==dif)return true;
+            }
+        }
+        top.clear();
+        bottom.clear();
+        sum1=0;
+        sum2=total;
+        for( int j=0;j<m;j++){
+            for( int i=0;i<n;i++){
+                int val=grid[i][j];
+                bottom[val]+=1;
+            }
+        }
+        for(int j=0;j<m;j++){
+            for(int i=0;i<n;i++){
+                int val=grid[i][j];
+                sum1+=val;
+                sum2-=val;
+                top[val]++;
+                bottom[val]--;
+                if(bottom[val]==0)bottom.erase(val);
+            }
+            if(sum1==sum2)return true;
+            long dif=abs(sum1-sum2);
+            if(sum1>sum2){
+                if(top.find(dif)!=top.end()){
+                    if(n!=1){
+                        if(j!=0   )return true;
+                        if(grid[0][0]==dif || grid[n-1][0]==dif)return true;
+                    }
+                    else if(grid[0][0]==dif ||grid[0][j]==dif)return true;
+                }
+                
+            }
+            else if( bottom.find(dif)!=bottom.end()){
+                if(n!=1){
+                    if(j!=m-2)return true;
+                    if(grid[0][m-1]==dif || grid[n-1][m-1]==dif)return true;
+                }
+                else if(grid[0][j+1]==dif || grid[0][m-1]==dif)return true;
+            }
+        }
+        return false;
+        }
+};
