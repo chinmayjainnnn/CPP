22a3d168f193c364277ccfc798e3a9fb3753252c
Chinmay Jain <149592825+chinmayjainnnn@users.noreply.github.com>
2026-03-29 13:39:41 +0530

    Time: 8 ms (59.33%) | Memory: 19.5 MB (95.33%) - LeetSync

--- 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -8,19 +8,14 @@ public:
         for(int i=0;i<n;i++){
             if(i%2){
                 odd[s1[i]-'a']+=1;
-            }
-            else{
-                even[s1[i]-'a']+=1;
-            }
-        }
-        for(int i=0;i<n;i++){
-            if(i%2){
                 odd[s2[i]-'a']-=1;
             }
             else{
+                even[s1[i]-'a']+=1;
                 even[s2[i]-'a']-=1;
             }
         }
+        
         for(int i=0;i<26;i++){
             if(even[i]!=0 ||odd[i]!=0)return false;
         }
