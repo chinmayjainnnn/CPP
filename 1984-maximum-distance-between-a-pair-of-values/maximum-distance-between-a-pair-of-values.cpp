0e146f82bef907e97ab8b7573f1e01b34153f451
Chinmay Jain <149592825+chinmayjainnnn@users.noreply.github.com>
2026-04-19 13:57:47 +0530

    Time: 0 ms (100.00%) | Memory: 102.2 MB (74.44%) - LeetSync

--- 1984-maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.cpp
+++ 1984-maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.cpp
@@ -5,16 +5,14 @@ public:
         int i=0,j=0;
         int ans=0;
         while(i<n&&j<m){
-            // int a=nums1[i],b=nums2[j];
-            if(i<=j && nums1[i]<=nums2[j])ans=max(j-i,ans);
-            if(nums1[i]>nums2[j]){
+            int a=nums1[i],b=nums2[j];
+            if(i<=j && a<=b)ans=max(j-i,ans);
+            if(a>b){
                 i++;
             }
             else{
                 j++;
             }
-            // cout<< i<<" "<<j<<endl;
-
         }
         return ans;
     }
