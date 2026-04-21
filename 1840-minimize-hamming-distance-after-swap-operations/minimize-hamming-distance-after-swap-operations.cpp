7395200f3a3f1a50f1c2b1c7c56ff21bcc7206cf
Chinmay Jain <149592825+chinmayjainnnn@users.noreply.github.com>
2026-04-22 00:54:48 +0530

    Time: 151 ms (61.78%) | Memory: 156.5 MB (36.12%) - LeetSync

--- 1840-minimize-hamming-distance-after-swap-operations/minimize-hamming-distance-after-swap-operations.cpp
+++ 1840-minimize-hamming-distance-after-swap-operations/minimize-hamming-distance-after-swap-operations.cpp
@@ -1,86 +1,48 @@
-// class Solution {
-// public:
-//     vector<int> parent;
-//     vector<int> rank;
-//     int find(int x){
-//         if(parent[x]!=x){
-//             parent[x]=find(parent[x]);
-//         }
-//         return parent[x];
-//     }
-//     void merge(int a,int b){
-//         int ra=find(a),rb=find(b);
-//         if(ra==rb)return;
-//         if(rank[ra]<rank[rb])parent[ra]=rb;
-//         else if(rank[rb]<rank[ra])parent[rb]=ra;
-//         else{
-//             parent[rb]=ra;
-//             rank[ra]++;
-//         }
-//     }
-//     int minimumHammingDistance(vector<int>& source, vector<int>& target, vector<vector<int>>& a) {
-//         int n=source.size();
-//         for(int i=0;i<n;i++){
-//             parent.push_back(i);
-//             rank.push_back(0);
-//         }
-//         for(auto it:a){
-//             merge(it[0],it[1]);
-//         }
-//         unordered_map<int,vector<int>> mp;
-//         for(int i=0;i<n;i++){
-//             int x=find(i);
-//             mp[source[x]].push_back(source[i]);
-//         }
-//         for(auto it:mp){
-//             for(auto i:it.second){
-//                 cout<<i<<" ";
-//             }
-//             cout<<endl;
-//         }
-//         return 0;
-//     }
-// };
-
 class Solution {
 public:
     vector<int> parent;
-
-    int find(int x) {
-        if (parent[x] != x) parent[x] = find(parent[x]);
+    vector<int> rank;
+    int find(int x){
+        if(parent[x]!=x){
+            parent[x]=find(parent[x]);
+        }
         return parent[x];
     }
-
-    void unite(int a, int b) {
-        parent[find(a)] = find(b);
+    void merge(int a,int b){
+        int ra=find(a),rb=find(b);
+        if(ra==rb)return;
+        if(rank[ra]<rank[rb])parent[ra]=rb;
+        else if(rank[rb]<rank[ra])parent[rb]=ra;
+        else{
+            parent[rb]=ra;
+            rank[ra]++;
+        }
     }
-
-    int minimumHammingDistance(vector<int>& source, vector<int>& target, vector<vector<int>>& allowedSwaps) {
-        int n = source.size();
-        parent.resize(n);
-        iota(parent.begin(), parent.end(), 0);
-
-        for (auto& swap : allowedSwaps) {
-            unite(swap[0], swap[1]);
+    int minimumHammingDistance(vector<int>& source, vector<int>& target, vector<vector<int>>& a) {
+        int n=source.size();
+        for(int i=0;i<n;i++){
+            parent.push_back(i);
+            rank.push_back(0);
         }
-
-        // Group source values by their component root
-        unordered_map<int, unordered_map<int, int>> groups;
-        for (int i = 0; i < n; i++) {
-            groups[find(i)][source[i]]++;
+        for(auto it:a){
+            merge(it[0],it[1]);
         }
-
-        int hammingDist = 0;
-        for (int i = 0; i < n; i++) {
-            int root = find(i);
-            auto& freq = groups[root];
-            if (freq.count(target[i]) && freq[target[i]] > 0) {
-                freq[target[i]]--;  // matched, consume this source value
-            } else {
-                hammingDist++;      // no match found in this component
+        unordered_map<int,unordered_map<int,int>> ump;
+        for(int i=0;i<n;i++){
+            ump[find(i)][source[i]]++;
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            int par=find(i);
+            auto &freq=ump[par];
+            int val=target[i];
+            if(freq.count(val) && freq[val]>0 ){
+                freq[val]--;
+            }
+            else{
+                ans++;
             }
         }
-
-        return hammingDist;
+        return ans;
     }
 };
