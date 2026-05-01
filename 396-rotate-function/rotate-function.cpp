4315e497f04eeca1acc60c73b42f88b53696fadd
Chinmay Jain <149592825+chinmayjainnnn@users.noreply.github.com>
2026-05-01 09:35:35 +0530

    Time: 7 ms (21.85%) | Memory: 104.6 MB (13.54%) - LeetSync

---
+++ 396-rotate-function/rotate-function.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxRotateFunction(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> dp(n,0);
+        int sum=0;
+        for(int i=0;i<n;i++){
+            dp[0]+=i*nums[i];
+            sum+=nums[i];
+        }
+        // cout<<"a"endl;
+        int maxi=dp[0];
+        for(int i=1;i<n;i++){
+            dp[i]=dp[i-1]+sum-(n*nums[n-i]);
+            maxi=max(maxi,dp[i]);
+            // cout<<dp[i]<<" "<<maxi<<endl;
+        }
+        return maxi;
+    }
+};
