22eee1b7ada1bd975d93155d5416004a5070b3d1
Chinmay Jain <149592825+chinmayjainnnn@users.noreply.github.com>
2026-05-06 12:24:12 +0530

    Time: 4 ms (95.33%) | Memory: 56.9 MB (15.72%) - LeetSync

--- 1972-rotating-the-box/rotating-the-box.cpp
+++ 1972-rotating-the-box/rotating-the-box.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
-    vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
-        int n=box.size(),m=box[0].size();
-        vector<vector<char>> rot(m,vector<char>(n));
-
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& b) {
+        int n=b.size(),m=b[0].size();
+        int ind;
+        vector<vector<char>> ans(m,vector<char>(n,'.'));
         for(int i=0;i<n;i++){
-            int obstacle=m,fill=m-1;
+            ind=m-1;
             for(int j=m-1;j>=0;j--){
-                if(box[i][j]=='*'){
-                    int temp=fill;
-                    obstacle=j;
-                    fill=j-1;
-                    while(obstacle<temp){
-                        rot[temp--][n-1-i]='.';
-                    }
-
-                    rot[j][n-1-i]='*';
+                if(b[i][j]=='#'){
+                    ans[ind--][n-1-i]='#';
                 }
-                else if(box[i][j]=='#'){
-                    rot[fill--][n-1-i]='#';
-                }                
-            }
-            while(fill>=0){
-                rot[fill--][n-1-i]='.';
+                else if(b[i][j]=='*'){
+                    
+                    ans[j][n-1-i]='*';
+                    ind=j-1;
+                }
+                
+                
             }
         }
-        return rot;
+        return ans;
     }
 };
