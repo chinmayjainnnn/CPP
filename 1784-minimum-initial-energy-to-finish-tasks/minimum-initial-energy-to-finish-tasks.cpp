fcb15047e2824c3cb648fed6269d2c34abe5dbe9
Chinmay Jain <149592825+chinmayjainnnn@users.noreply.github.com>
2026-05-17 17:15:39 +0530

    Time: 26 ms (92.05%) | Memory: 97.6 MB (33.39%) - LeetSync

--- 1784-minimum-initial-energy-to-finish-tasks/minimum-initial-energy-to-finish-tasks.cpp
+++ 1784-minimum-initial-energy-to-finish-tasks/minimum-initial-energy-to-finish-tasks.cpp
@@ -3,13 +3,13 @@ public:
     
     int minimumEffort(vector<vector<int>>& tasks) {
         int n=tasks.size();
-        int ans=0,last=0;
+        int ans=0;
         sort(tasks.begin(),tasks.end(),[](const vector<int> &a,const vector<int> &b){
         return a[1]-a[0]>b[1]-b[0];
     });
         for(int i=n-1;i>=0;i--){
-            ans=ans+tasks[i][0];
-            ans=max(ans,tasks[i][1]);
+            
+            ans=max(ans+tasks[i][0],tasks[i][1]);
         }
         return ans;
     }
