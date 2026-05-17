51283caa596f222f9c29a03f9665606785157c20
Chinmay Jain <149592825+chinmayjainnnn@users.noreply.github.com>
2026-05-17 11:12:13 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (27.29%) - LeetSync

--- 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        return *min_element(nums.begin(),nums.end());
+        int n=nums.size();
+        int s=0,e=n-1,mid,ans=nums[n-1];
+        while(e>=s){
+            mid=s+(e-s)/2;
+            int x=nums[mid];
+            ans=min(ans,x);
+            if(x>nums[s]){ans=min(ans,nums[s]);s=mid+1;}
+            else if(x<nums[s])e=mid-1;
+            else s++;
+        }
+        return ans;
     }
 };
