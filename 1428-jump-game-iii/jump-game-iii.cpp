fc737475608c4c15fa0af1d3990243a7c9071227
Chinmay Jain <149592825+chinmayjainnnn@users.noreply.github.com>
2026-05-17 20:14:46 +0530

    Time: 0 ms (100.00%) | Memory: 35.3 MB (85.60%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.cpp
+++ 1428-jump-game-iii/jump-game-iii.cpp
@@ -2,19 +2,20 @@ class Solution {
 public:
     bool canReach(vector<int>& arr, int start) {
        int n=arr.size();
-       vector<int> vis(n,0);
+       vector<bool> vis(n,0);
        queue<int> q;
         q.push(start);
-        vis[start]=1;
+        vis[start]=true;
        while(!q.empty()){
             int cur=q.front();
-            int val=arr[cur];
-            if(val==0)return true;
             q.pop();
+            if(arr[cur]==0)return true;
+            int val=arr[cur];
+            
 
-            if(cur-val>=0 && !vis[cur-val]){q.push(cur-val);vis[cur-val]=1;}
-            if(cur+val<n && !vis[cur+val]){q.push(cur+val);vis[cur+val]=1;}
+            if(cur-val>=0 && !vis[cur-val]){q.push(cur-val);vis[cur-val]=true;}
+            if(cur+val<n && !vis[cur+val]){q.push(cur+val);vis[cur+val]=true;}
        }
-       return 0;
+       return false;
     }
 };
